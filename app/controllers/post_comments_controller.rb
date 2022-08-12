c40dae55681f19488ea9af0cde13ad9bcf2129cd
EC2 Default User <ec2-user@ip-172-31-20-243.ap-northeast-1.compute.internal>
2022-08-12 06:17:43 +0000

    コメント機能非同期通信未達成

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,13 +4,15 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    @books=Book.find(params[:book_id])
+    @post_comment = PostComment.new
+    
   end
 
   def destroy
-    post_comment = PostComment.find(params[:book_id])
-    post_comment.destroy
-    redirect_to book_path
+    @book=Book.find(params[:id])
+    @post_comment=PostComment.find_by(id: params[:id], book_id: params[:book_id])
+    @post_comment.destroy
   end
 
   private
