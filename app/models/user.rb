aa38f331bddaeea9829f943d107e0c2489cbb92c
EC2 Default User <ec2-user@ip-172-31-20-243.ap-northeast-1.compute.internal>
2022-08-26 06:48:48 +0000

    お互いフォローしていないとDMできない機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,10 @@ class User < ApplicationRecord
     reverse_of_relationships.find_by(following_id: user.id).present?
   end
   
+  def following?(user)
+    followings.include?(user)
+  end
+  
   # 検索方法分岐
   def self.looks(search, word)
     if search == "perfect_match"
